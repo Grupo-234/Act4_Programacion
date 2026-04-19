bc1dcfff8c02672c54b0f58f533f50feb0a6771e
lucod3rs <ing.luiscuaical@gmail.com>
2026-04-18 21:08:47 -0500

    #Actualizacion Commits

--- SlnSistema.py
+++ SlnSistema.py
@@ -2,9 +2,4 @@
 ##Programacion orientada a objetos
 ##Grupo: 213023_234
 
-import tkinter as tk
-from tkinter import messagebox
-
-print("Bienvenido al sistema")
-#Procedimiento para mostrar el menú de opciones
-#Prueba conexion git visual studio code
+#Actualizacion Commit desde Visul Studio Code
