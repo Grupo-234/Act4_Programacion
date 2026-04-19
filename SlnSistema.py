c1fc2a5a2b907491353a5ab2bcdc366af7a5c4f8
angelabravogomez-max <angelabravogomez@gmail.com>
2026-04-18 23:18:22 -0500

    Prueba commit Angela

--- SlnSistema.py
+++ SlnSistema.py
@@ -3,10 +3,6 @@
 ##Grupo: 213023_234
 #Actualizacion Commit desde Visul Studio Code
 
-#Importar librerias del sistema
-import _tkinter as tk
-from tkinter import messagebox
-
 num1 = int(input("Ingrese el primer número: "))
 num2 = int(input("Ingrese el segundo número: "))
 num3 = int(input("Ingrese el tercer número: "))
@@ -21,11 +17,7 @@ print("La resta de", num1, "y", num2, "es:", resta)
 multiplicacion = num1 * num2
 print("La multiplicación de", num1, "y", num2, "es:", multiplicacion)
 
-division = num1 / num2
-print("La división de", num1, "y", num2, "es:", division)
-
-multiplicacion2 = num1 * num2 * num3
-print("La multiplicación de", num1, ",", num2, "y", num3, "es:", multiplicacion2)
+print("Prueba angela")
 
 #Definir la clase del sistema
 
