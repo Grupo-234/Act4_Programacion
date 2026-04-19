b91b56ad4679e4f5fe9e4e73572864922ee1d8a3
lucod3rs <ing.luiscuaical@gmail.com>
2026-04-18 21:14:21 -0500

    Add library imports and class definition placeholder

--- SlnSistema.py
+++ SlnSistema.py
@@ -1,5 +1,11 @@
 ##Componente práctico - Prácticas simuladas
 ##Programacion orientada a objetos
 ##Grupo: 213023_234
-
 #Actualizacion Commit desde Visul Studio Code
+
+#Importar librerias del sistema
+import _tkinter as tk
+from tkinter import messagebox
+
+#Definir la clase del sistema
+
