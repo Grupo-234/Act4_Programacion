57d5491993efd463b39c4b9ed230162c9ffeffcd
lucod3rs <ing.luiscuaical@gmail.com>
2026-05-01 19:30:04 -0500

    Metodo Abstracto

--- entidad.py
+++ entidad.py
@@ -17,3 +17,23 @@ class EntidadPersona(ABC):
         self._telefono = self._validar_telefono(telefono)
         self._correo = correo
         self._estado = "Activo"
+ 
+###################################################
+#Metodo Abstracto. Metodo obligatorio para quienes hereden de esta clase
+###################################################
+ 
+    @abstractmethod
+    def mostrar_perfil(self): 
+        pass
+
+###################################################
+# Validaciones específicas para los campos de la entidad
+###################################################
+
+    def _validar_documento(self, valor):
+        # Implementamos la lógica de "Solo números"
+        if not str(valor).isdigit():
+            msg = f"Error Crítico: El documento '{valor}' no es numérico."
+            registrar_evento(msg, nivel="error")
+            raise ErrorValidacionDatos(msg)
+        return valor
