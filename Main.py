fffc5811941c7c35234aa933b9cc1a7c158c84ac
LuisC <ing.luiscuaical@gmail.com>
2026-05-06 21:43:21 -0500

    Main / logging registro eventos

--- Main.py
+++ Main.py
@@ -2,10 +2,21 @@
 # Importamos las librerías necesarias para la aplicación
 ##########################
 
-import tkinter as tk
-from tkinter import ttk, messagebox
-from datetime import datetime, timedelta
-import re
-import logging
-import csv
-import os
+import tkinter as tk # Interfaz gráfica de usuario
+from tkinter import ttk, messagebox # ttk para estilos de widgets, messagebox para mostrar mensajes emergentes
+from datetime import datetime, timedelta # datetime para manejar fechas y horas, timedelta para realizar operaciones con fechas
+import re # re para expresiones regulares, útil para validar entradas de usuario
+import logging # logging para registrar eventos y errores en la aplicación
+import csv # csv para manejar archivos CSV, útil para guardar y cargar datos de usuarios
+import os # os para interactuar con el sistema operativo, como verificar la existencia de archivos o crear directorios
+
+##########################
+# Configuración del logging para registrar eventos y errores
+##########################
+logging.basicConfig(
+    filename='registro_eventos.log', 
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    force=True
+)
