3020e574dca5c0c9a1e6b09c7a5f040d363da93f
LuisC <ing.luiscuaical@gmail.com>
2026-05-06 21:17:38 -0500

    Metodo Gestion Reserva

--- modelos/reserva.py
+++ modelos/reserva.py
@@ -20,3 +20,30 @@ class Reserva:
         self.cantidad = cantidad # Cantidad de unidades del servicio que se están reservando
         self.fecha = fecha        # Fecha de la reserva
         self.total = servicio.calcular_precio(cantidad) # Cálculo del precio total de la reserva basado en el servicio y la cantidad
+
+
+###########################
+# Métodos para gestionar la reserva
+############################
+    def confirmar_reserva(self):
+        """Genera el mensaje para el log y confirma el proceso."""
+        resumen_log = (
+            f"Proceso Exitoso - Cliente: {self.cliente._nombre} | "
+            f"Servicio: {self.servicio._nombre_servicio} | Total: {self.total}"
+        )
+        registrar_evento(resumen_log, nivel="info")
+        return "Reserva procesada y registrada exitosamente."
+    
+    def __str__(self):
+        """Trae la información de cliente, servicio y reserva."""
+        return (
+            f"\n==============================="
+            f"\n       COMPROBANTE DE RESERVA  "
+            f"\n==============================="
+            f"\nFECHA: {self.fecha}"
+            f"\nCLIENTE: {self.cliente._nombre} ({self.cliente._tipo_doc}: {self.cliente._num_doc})"
+            f"\nSERVICIO: {self.servicio._nombre_servicio}"
+            f"\nCANTIDAD: {self.cantidad}"
+            f"\nTOTAL A PAGAR: ${self.total}"
+            f"\n===============================\n"
+        )
